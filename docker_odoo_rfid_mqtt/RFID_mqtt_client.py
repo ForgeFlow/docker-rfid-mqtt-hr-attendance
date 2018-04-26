7aa61fadd277cf63ce9e845d4092450fb7383269
lurobe94 <luis.romero@eficent.com>
2018-04-26 09:44:21 +0200

    fixing logins and secrets

--- docker_odoo_rfid_mqtt/RFID_mqtt_client.py
+++ docker_odoo_rfid_mqtt/RFID_mqtt_client.py
@@ -5,14 +5,41 @@ import paho.mqtt.client as mqtt
 import os
 import urlparse
 
-user_id = os.environ.get('USERID')
-user_password = os.environ.get('DB_PASSWORD')
-object_facade = None
-dbname = os.environ.get('PGDATABASE')
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256, HMAC
+import binascii
+import base64
+import random, time
 
-mqtt_id = os.environ.get('MQTTHOST')
-mqtt_user = os.environ.get('MQTTUSER')
-mqtt_pass = os.environ.get('MQTTPASS')
+object_facade = None
+
+d = {}
+
+with open("./secrets/sensitiveInfo", "r") as sensInfo:
+
+	#textfile = sensInfo.read().splitlines()
+    for line in sensInfo:
+        key,val = line.split(":")
+        d[str(key)] = val[:-1]
+        print("KEY: " + str(key) + "|" + "VAL: " + str(val))
+
+user_id = d["user_id"]
+host = d["host"]
+port = d["port"]
+user_name = d["user_name"]
+user_password = d["user_password"]
+dbname = d["dbname"]
+mqtt_id = d["mqtt_id"]
+mqtt_user = d["mqtt_user"]
+mqtt_pass = d["mqtt_pass"]
+#ivkey = d["ivkey"]
+key = d["key"]
+staticiv = d["staticiv"]
+
+cnt = 0
+r = 0
+same = True
+
 
 def connection(host, port, user, user_pw, database):
     global user_password
@@ -89,6 +116,16 @@ def registro(employeeData):
     except Exception as e:
         return "FALSE"
 
+def compare_digest(x, y):
+    if not (isinstance(x, bytes) and isinstance(y, bytes)):
+        raise TypeError("both inputs should be instances of bytes")
+    if len(x) != len(y):
+        return False
+    result = 0
+    for a, b in zip(x, y):
+        result |= ord(a) ^ ord(b)
+    return result == 0
+
 
 # Overwrites MQTT events
 
@@ -99,18 +136,115 @@ def on_connect(mosq, obj, rc):
 
 # Is executed on new message
 def on_message(mosq, obj, msg):
-    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-    host, port, user, user_pw, database, card_id = str(msg.payload).split("###")
-    connection(host, port, user, user_pw, database)
-    cons = consulta(card_id)
 
-    if (cons["employeeId"] != ""):
-        print(cons)
-        retorno = registro(cons)
-        print(retorno)
-        mqttc.publish("retorno", retorno)
-    else:
-        mqttc.publish("retorno", "FALSE")
+	global cnt
+
+	global r, same, flag_auth
+
+	global host, port, user_name, user_password, dbname
+
+	if msg.topic == 'reset':
+
+		cnt = 0;
+		iv_t = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+		print("RESET!!!!!!!!!!!!!!!")
+		
+
+	elif msg.topic == 'acceso':
+
+		if same == False or flag_auth == True:
+			mqttc.publish("retorno", "NOAUTH")
+			print("++++++++++++++HMAC authentication failed++++++++++++++")
+		else:
+			flag_auth = True
+			same = False
+			print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+			card_id_b64 = msg.payload
+			
+			card_id_aux = base64.b64decode(card_id_b64)
+
+			print("CARD (encryptd and coded): " + card_id_aux)
+			decryption_suite = AES.new(key,AES.MODE_CBC,r)
+			card_id_aux = decryption_suite.decrypt(card_id_aux)
+			print("CARD (coded): " + card_id_aux)
+			card_id_aux = base64.b64decode(card_id_aux)
+
+			print("ID: " + card_id_aux)
+
+			card_id = card_id_aux[-8:]
+
+			print("#################################################################################")
+			print("PARAMETERS: " + str(host) + " / " + str(port) + " / " + str(user_name) + " / " + str(user_password) + " / " + str(dbname))
+			print("#################################################################################")
+			connection(host, port, user_name, user_password, dbname)
+
+			if check_id_integrity(card_id):
+
+				cons = consulta(card_id)
+
+				if (cons["employeeId"] != ""):
+					print(cons)
+					retorno = registro(cons)
+					print(retorno)
+					mqttc.publish("retorno", retorno)
+					r = os.urandom(16)
+					r = set_range(r)
+				
+				else:
+					mqttc.publish("retorno", "FALSE")
+
+			else:
+				print("--------------- CARD ID INTEGRITY COMPROMISED ----------------")
+				mqttc.publish("retorno", "NOAUTH")
+
+		print("Trial: " + str(cnt))
+		cnt = cnt + 1
+
+	elif msg.topic == "hmac":
+		print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+		hmac = HMAC.new(key,r,SHA256)
+		print key
+		computed_hash_hex = hmac.hexdigest()
+		print("HMAC(HEX): " + computed_hash_hex)
+
+		hashb64 = base64.b64decode(msg.payload)
+		hash_from_arduino = hashb64.encode("hex")
+		print("HMAC_ESP8266(HEX): " + hash_from_arduino)
+
+		same = compare_digest(computed_hash_hex,hash_from_arduino)
+		flag_auth = False
+		print("HMAC Comparison: " + str(same))
+
+	else:
+		r = os.urandom(16)
+		r = set_range(r)
+		mqttc.publish("ack", r)
+		print("Session ID: " + r)
+
+
+def set_range(l):
+	for i in xrange(len(l)):
+		s = l[i]
+		while (s<'!' or s>'}'):
+			s = os.urandom(1)
+		if i == 0:
+			p = s
+		else:
+			p = p + s
+	return str(p)
+
+def check_id_integrity(nid):
+	checksum = 0
+	for i in xrange(len(nid)):
+		#print("ID[" + str(i) + "]: " + nid[i])
+		if (nid[i]>='0' and nid[i]<='9') or (nid[i]>='a' and nid[i]<='z'):
+			checksum = checksum + 1
+		#print("Sum[" + str(i) + "]: " + str(checksum))
+	if checksum == 8:
+		return True
+	else:
+		return False
 
 
 # Is executed to send messages
@@ -137,16 +271,23 @@ mqttc.on_connect = on_connect
 mqttc.on_publish = on_publish
 mqttc.on_subscribe = on_subscribe
 
+
 # user and password cloudmqtt
+
 mqttc.username_pw_set(mqtt_user, mqtt_pass)
 # CLOUDMQTT url where the rows to read stay
 mqttc.connect(mqtt_id, 1883)
 
 # subscribe to topic
-mqttc.subscribe("acesso", 0)
+mqttc.subscribe("acceso", 0)
+mqttc.subscribe("reset",0)
+
+mqttc.subscribe("init",0)
+mqttc.subscribe("hmac",0)
 
 # loop searching for new data to read
 rc = 0
 while rc == 0:
     rc = mqttc.loop()
 print("rc: " + str(rc))
+
--- nodemcu_rfid_wifimanager_localhost/nodemcu_rfid_wifimanager_localhost.ino
+++ nodemcu_rfid_wifimanager_localhost/nodemcu_rfid_wifimanager_localhost.ino
@@ -1,4 +1,4 @@
-#include <FS.h>                   //this needs to be first, or it all crashes and burns...
+ #include <FS.h>                   //this needs to be first, or it all crashes and burns...
 
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 
@@ -12,47 +12,67 @@
 //https://github.com/knolleary/pubsubclient
 #include <PubSubClient.h>
 
-#include <AES.h>
-#include <AES_config.h>
 #include <ebase64.h>
+#include <AES_config.h>
+#include <AES.h>
+
+#include <Crypto.h>
 
 #include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
-#define RESET_PIN 16
+#define RESET_PIN 16  // -> CHANGE TO D3 - GPIO0
 #define RED_LED 4
 #define GREEN_LED 5
-#define BEEP 10
+#define BEEP 15
 
 #include <SPI.h>
 #include "MFRC522.h"
-#define RST_PIN 0 // RST-PIN for RC522 - RFID - SPI - Modul GPIO0 - D3
-#define SS_PIN  2  // SDA-PIN for RC522 - RFID - SPI - Modul GPIO2 - D4
+#define RST_PIN 0 // RST-PIN for RC522 - RFID - SPI - Modul GPIO0 - D3  --> CHANGE TO D8 - GPIO15?
+#define SS_PIN 2  // SDA-PIN for RC522 - RFID - SPI - Modul GPIO2 - D4  
 
+/* The key can be of any length, 16 and 32 are common */
+#define KEY_LENGTH 16
 
-//define your default values here, if there are different values in config.json, they are overwritten.
+int cnt, cnt_r, cnt_rold;
+
+/* Define our */
+byte key_hmac[KEY_LENGTH] = {77, 49, 107, 51, 121, 49, 115, 100, 65, 98, 51, 83, 116, 48, 110, 51}; // 'M1k3y1sdAb3St0n3'
+
+byte authCode[SHA256HMAC_SIZE];
+char authCodeb64[200];
+
+//define your default values here, 
+//if there are different values in config.json, 
+//they are overwritten.
 //length should be max size + 1
-char odoo_host[90];
-char odoo_port[6] = "8069";
-char odoo_user[33] = "admin";
-char odoo_password[33] = "admin";
-char odoo_database[24];
 
-//MQTT parametros.
-const char* mqtt_server = "192.168.1.37";
-const int mqtt_port = 1883;
+char mqtt_server[15];
+
+char nodeMCUClient[15];
+char userMQTT[15];
+char passwordMQTT[15];
+
+char buf[512];
+
+char rfidstr[15];
+ char rfid_b64[200];
+
+int flag_init = 1;
 
 //AES parameters
 
 AES aes;
 
-// Our AES key.
-byte key[] = {0x7e, 0x4e, 0x42, 0x38, 0x43, 0x63, 0x4f, 0x4c, 0x23, 0x4a, 0x21, 0x48, 0x3f, 0x7c, 0x59, 0x72};
+char key[20];//N_BLOCK+1]="M1k3y1sdAb3St0n3";//"76rTy8aaSc34dLqw"; // key for the AES encryption of the payload
+char iv_py[20];
 
-// The initialized Initialization vector
-byte iv[N_BLOCK] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+//MQTT parametros.
+//const char* mqtt_server = "192.168.1.37";
+const int mqtt_port = 1883;
 
 //flag for saving data
-bool shouldSaveConfig = false;
+bool shouldSaveConfig = true;
 String currentCard = "";
+String currentCardold = "";
 
 WiFiManager wifiManager;
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
@@ -69,15 +89,15 @@ void saveConfigCallback () {
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(115200);                      
   Serial.println();
 
-  pinMode(RESET_PIN, INPUT);
+  pinMode(RESET_PIN, INPUT); 
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
   SPI.begin();           // Init SPI bus
   mfrc522.PCD_Init();    // Init MFRC522
-
+  Serial.println("MFRC522 Initialized");
   //clean FS, for testing
   //SPIFFS.format();
 
@@ -102,12 +122,13 @@ void setup() {
         json.printTo(Serial);
         if (json.success()) {
           Serial.println("\nparsed json");
-
-          strcpy(odoo_host, json["odoo_host"]);
-          strcpy(odoo_port, json["odoo_port"]);
-          strcpy(odoo_user, json["odoo_user"]);
-          strcpy(odoo_password, json["odoo_password"]);
-          strcpy(odoo_database, json["odoo_database"]);
+          Serial.println("\nHASTA AQUI 1");
+          strcpy(mqtt_server, json["mqtt_server"]);
+          strcpy(key, json["key"]);
+          strcpy(nodeMCUClient, json["nodeMCUClient"]);
+          strcpy(userMQTT, json["userMQTT"]);
+          strcpy(passwordMQTT, json["passwordMQTT"]);
+          Serial.println("\nHASTA AQUI 2");
         } else {
           Serial.println("failed to load json config");
         }
@@ -117,21 +138,22 @@ void setup() {
     Serial.println("failed to mount FS");
   }
   //end read
-  Serial.println(odoo_host);
-  Serial.println(odoo_port);
-  Serial.println(odoo_user);
-  Serial.println(odoo_password);
-  Serial.println(odoo_database);
-
+  
+  Serial.println(mqtt_server);
+  Serial.println(key);
+  Serial.println(nodeMCUClient);
+  Serial.println(userMQTT);
+  Serial.println(passwordMQTT);
 
   // The extra parameters to be configured (can be either global or just in the setup)
   // After connecting, parameter.getValue() will get you the configured value
   // id/name placeholder/prompt default length
-  WiFiManagerParameter custom_odoo_host("host", "Odoo host", odoo_host, 89);
-  WiFiManagerParameter custom_odoo_port("port", "Odoo port", odoo_port, 5);
-  WiFiManagerParameter custom_odoo_user("user", "Odoo user", odoo_user, 32);
-  WiFiManagerParameter custom_odoo_password("password", "Odoo password", odoo_password, 32);
-  WiFiManagerParameter custom_odoo_database("database", "Odoo database", odoo_database, 23);
+
+  WiFiManagerParameter custom_mqtt_server("mqtt_server", "MQTT Server", mqtt_server, 14);
+  WiFiManagerParameter custom_key("key", "AES key", key, 19);
+  WiFiManagerParameter custom_nodeMCUClient("nodeMCUClient", "NodeMCU Client", nodeMCUClient, 14);
+  WiFiManagerParameter custom_userMQTT("userMQTT", "MQTT Username", userMQTT, 14);
+  WiFiManagerParameter custom_passwordMQTT("passwordMQTT", "MQTT Password", passwordMQTT, 14);
 
   //WiFiManager
   //Local intialization. Once its business is done, there is no need to keep it around
@@ -141,11 +163,12 @@ void setup() {
   wifiManager.setSaveConfigCallback(saveConfigCallback);
 
   //add all your parameters here
-  wifiManager.addParameter(&custom_odoo_host);
-  wifiManager.addParameter(&custom_odoo_port);
-  wifiManager.addParameter(&custom_odoo_user);
-  wifiManager.addParameter(&custom_odoo_password);
-  wifiManager.addParameter(&custom_odoo_database);
+
+  wifiManager.addParameter(&custom_mqtt_server);
+  wifiManager.addParameter(&custom_key);
+  wifiManager.addParameter(&custom_nodeMCUClient);
+  wifiManager.addParameter(&custom_userMQTT);
+  wifiManager.addParameter(&custom_passwordMQTT);
 
   //reset settings - for testing
   //wifiManager.resetSettings();
@@ -153,44 +176,54 @@ void setup() {
   //set minimu quality of signal so it ignores AP's under that quality
   //defaults to 8%
   wifiManager.setMinimumSignalQuality();
-
   //sets timeout until configuration portal gets turned off
   //useful to make it all retry or go to sleep
   //in seconds
-  //wifiManager.setTimeout(120);
+  wifiManager.setTimeout(120);
 
   //fetches ssid and pass and tries to connect
   //if it does not connect it starts an access point with the specified name
   //here  "AutoConnectAP"
   //and goes into a blocking loop awaiting configuration
+
   if (!wifiManager.autoConnect("AutoConnectAP", "12345678")) {
     Serial.println("failed to connect and hit timeout");
     delay(3000);
     //reset and try again, or maybe put it to deep sleep
     ESP.reset();
     delay(5000);
+  } else {
+    Serial.println("password:12345678");
   }
 
   //if you get here you have connected to the WiFi
-  Serial.println("connected...yeey :)");
+  Serial.println("*CONNECTED*");
 
   //read updated parameters
-  strcpy(odoo_host, custom_odoo_host.getValue());
-  strcpy(odoo_port, custom_odoo_port.getValue());
-  strcpy(odoo_user, custom_odoo_user.getValue());
-  strcpy(odoo_password, custom_odoo_password.getValue());
-  strcpy(odoo_database, custom_odoo_database.getValue());
+
+  strcpy(mqtt_server, custom_mqtt_server.getValue());
+  strcpy(key, custom_key.getValue());
+  strcpy(nodeMCUClient, custom_nodeMCUClient.getValue());
+  strcpy(userMQTT, custom_userMQTT.getValue());
+  strcpy(passwordMQTT, custom_passwordMQTT.getValue());
 
   //save the custom parameters to FS
   if (shouldSaveConfig) {
     Serial.println("saving config");
     DynamicJsonBuffer jsonBuffer;
     JsonObject& json = jsonBuffer.createObject();
-    json["odoo_host"] = odoo_host;
-    json["odoo_port"] = odoo_port;
-    json["odoo_user"] = odoo_user;
-    json["odoo_password"] = odoo_password;
-    json["odoo_database"] = odoo_database;
+
+    json["mqtt_server"] = mqtt_server;
+    json["key"] = key;//"M1k3y1sdAb3St0n3";
+    json["nodeMCUClient"] = nodeMCUClient;
+    json["userMQTT"] = userMQTT;
+    json["passwordMQTT"] = passwordMQTT;   
+
+    Serial.println("+++++++++++++++++");
+
+    Serial.println(nodeMCUClient);
+    Serial.println(userMQTT);
+    Serial.println(passwordMQTT);
 
     File configFile = SPIFFS.open("/config.json", "w");
     if (!configFile) {
@@ -202,6 +235,12 @@ void setup() {
     configFile.close();
     //end save
   }
+
+  CharToByte(key, key_hmac, KEY_LENGTH);
+
+  Serial.println("KEY HMAC + KEY : **********");
+  Serial.println((char*)key_hmac);
+  Serial.println(key);
 
   Serial.println("local ip");
   Serial.println(WiFi.localIP());
@@ -215,8 +254,12 @@ void setup() {
   client.setCallback(callback);
 
   Serial.println(F("Ready!"));
-  Serial.println(F("======================================================"));
-  Serial.println(F("Scan for Card and print UID:"));
+
+  Serial.println("#############################################################################");
+
+  cnt = 0;
+  cnt_r = 0;
+
 }
 
 void conectMqtt() {
@@ -224,51 +267,98 @@ void conectMqtt() {
     Serial.print("ConnectingMQTT ...");
 
     //Parameters nodeMCUClient, userMQTT, passwordMQTT
-    if (client.connect("esp8266", "mqtt_rfid", "password")) {
+    if (client.connect(nodeMCUClient,userMQTT,passwordMQTT)){//"esp8266","mqtt_rfid","password")) {
       Serial.println("Connected");
       //Subscriing to topic retorno.
       client.subscribe("retorno");
+      client.subscribe("ack");
     } else {
       Serial.print("Error");
       Serial.print(client.state());
       Serial.println("Retry in 5 seconds");
       // Wait 5 seconds before retrying
-      delay(5000);
+      delay(1000);
+      
     }
   }
 }
 
 //Parameters: topic_name, msg , msg length
 void callback(char* topic, byte* payload, unsigned int length) {
+  SHA256HMAC hmac(key_hmac, KEY_LENGTH);  
   Serial.println();
   Serial.print("Welcome [");
   Serial.print(topic);
   Serial.print("]: ");
   String mensagem = "";
+
   //Convert msg from byte to string
   for (int i = 0; i < length; i++) {
     mensagem += (char)payload[i];
   }
+  mensagem.toCharArray(iv_py, sizeof iv_py);
   Serial.println(mensagem);
   Serial.println();
-  if (mensagem == "FALSE") {
-    digitalWrite(RED_LED, HIGH);
-    delay(1000);
-    digitalWrite(RED_LED, LOW);
-    delay(250);
+  if(strcmp(topic, "retorno") == 0){
+    
+    cnt = 0;
+
+    if (mensagem == "FALSE") {
+      digitalWrite(RED_LED, HIGH);
+      tone(BEEP, 1930);
+        delay(150);
+        tone(BEEP, 1630);
+        delay(100);
+        noTone(BEEP);
+      delay(1000);
+      digitalWrite(RED_LED, LOW);
+      delay(250);
+    }
+    else {
+      if(mensagem == "NOAUTH"){
+        digitalWrite(RED_LED, HIGH);
+        delay(500);
+        digitalWrite(GREEN_LED, HIGH);
+        delay(500);
+        digitalWrite(RED_LED, LOW);
+        delay(250);
+        digitalWrite(GREEN_LED, LOW);
+        delay(250);
+      } else {
+        digitalWrite(GREEN_LED, HIGH);
+        tone(BEEP, 1630);
+        delay(150);
+        tone(BEEP, 1930);
+        delay(100);
+        noTone(BEEP);
+        delay(1000);
+        digitalWrite(GREEN_LED, LOW);
+        delay(250);
+        Serial.println("#############################################################################");
+    }
   }
-  else {
-    digitalWrite(GREEN_LED, HIGH);
-    tone(BEEP, 1630);
-    delay(150);
-    tone(BEEP, 1930);
-    delay(100);
-    noTone(BEEP);
-    delay(1000);
-    digitalWrite(GREEN_LED, LOW);
-    delay(250);
-  }
+  currentCardold = currentCard;
   currentCard = "";
+  Serial.println("current card emptied");
+  } else if(strcmp(topic, "ack") == 0){
+      Serial.println(" ");
+      Serial.println(iv_py);
+      Serial.println(" ");
+
+      hmac.doUpdate(iv_py,strlen(iv_py));
+
+      hmac.doFinal(authCode);
+      Serial.println("AUTH CODE");
+      Serial.println();
+      for (byte i=0; i < SHA256HMAC_SIZE; i++)
+      {
+        Serial.print("0123456789abcdef"[authCode[i]>>4]);
+        Serial.print("0123456789abcdef"[authCode[i]&0xf]);
+      }
+      Serial.println(" ");
+
+      flag_init = 0;
+  } else Serial.println("ELSE " + mensagem);
 }
 
 void loop() {
@@ -277,14 +367,20 @@ void loop() {
   }
   client.loop();
 
-  if ( digitalRead(RESET_PIN) == LOW ) {
-    Serial.println("RESET");
-    wifiManager.resetSettings();
-    delay(3000);
-    ESP.reset();
-    delay(5000);
+  if(flag_init){
+    client.publish("init","INIT");
+    flag_init = 0;
+    Serial.println("");
+    Serial.println("ACK ASKED");
+    Serial.println("");
   }
 
+  Serial.print("CURRENT RFID: " + String(currentCard));
+  Serial.println("");
+  cnt = cnt + 1;
+  cnt_r = cnt_r + 1;
+   Serial.print("CONTADOR: " + String(cnt));
+  Serial.println("");
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
     delay(50);
@@ -295,96 +391,80 @@ void loop() {
     delay(50);
     return;
   }
+  cnt_r = cnt_r - 1;
   // Show some details of the PICC (that is: the tag/card)
-  Serial.print(F("Card UID:"));
-  if (currentCard == "") {
+  Serial.println("");
+
+  if (currentCard == "" && cnt > 25) { // this cnt allows to make a wait between card reads 
+    base64_encode(authCodeb64, (char *)authCode, SHA256HMAC_SIZE);
+    client.publish("hmac",(char *)authCodeb64);
+
     dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
+    encrypt_rfid(rfidstr,iv_py);
+    Serial.println("");
+    Serial.print("CURRENT CARD: " + String(currentCard));
+    Serial.println("");
+    Serial.print("CURRENT CARD OLD: " + String(currentCardold));
+    
+    Serial.println("");
+    Serial.print("MESSAGE: " + String(rfid_b64));
+    if(currentCard != currentCardold || cnt > 60){ // this cnt allows to set the time between card reads for the same card
+      client.publish("acceso", rfid_b64);
+    } else {
+      currentCard = "";
+    }
+    memset(rfid_b64, 0, sizeof(rfid_b64));
+    memset(rfidstr, 0, sizeof(rfidstr));
+
     Serial.println();
+    flag_init = 1;
   }
+
+}
+
+void encrypt_rfid(char rfidstr[], char iv_py[])
+{
+  byte cipher_rfid[1000];
+  
+  int len = base64_encode(rfid_b64, rfidstr, strlen(rfidstr));
+  aes.do_aes_encrypt((byte *)rfid_b64, len, cipher_rfid, (byte *)key, 128, (byte *)iv_py);
+  base64_encode(rfid_b64, (char *)cipher_rfid, aes.get_size());
+
+  Serial.println("RFID B64: " + String((char *) rfid_b64));
+  Serial.println("");
 
 }
 
 // Helper routine to dump a byte array as hex values to Serial
 void dump_byte_array(byte *buffer, byte bufferSize) {
-  char rfidstr[15];
+  
+  String rfid;
+  char rfid_b[8];
   char s[100];
+  byte cipher_iv[1000];
+ 
   for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], HEX);
+    //Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    //Serial.print(buffer[i], HEX);
     //Convert from byte to Hexadecimal
     sprintf(s, "%s%x", buffer[i] < 0x10 ? "0" : "", mfrc522.uid.uidByte[i]);
     //Concatenate msg
     strcat( &rfidstr[i] , s);
   }
-  char buf[512];
-  char b64data[512];
-  //snprintf(buf, sizeof buf, "%s###%s###%s###%s###%s###%s", odoo_host, odoo_port, odoo_user, odoo_password, odoo_database, rfidstr);
-  encrypt(buf,b64data,rfidstr);
+  
   currentCard = rfidstr;
-  client.publish("acesso", b64data);
-  Serial.println(" Verifing...");
+  Serial.println(" ");
+  Serial.print("RFID: " + String(rfidstr));
+
+  rfid = String(rfidstr).substring(strlen(rfidstr)-8,strlen(rfidstr));
+   
+  Serial.println("");
+  Serial.println("Verifing...");
+  Serial.println("");
 }
 
-uint8_t generate_random_uint8()
-{
-  uint8_t really_random = *(volatile uint8_t *)0x3FF20E44;
-  return really_random;
+void CharToByte(char* chars, byte* bytes, unsigned int count){
+    for(unsigned int i = 0; i < count; i++)
+        bytes[i] = (byte)chars[i];
 }
-
-// Generate a random initialization vector
-void generate_iv(byte *vector)
-{
-  for (int i = 0; i < N_BLOCK; i++)
-  {
-    vector[i] = (byte)generate_random_uint8();
-  }
-}
-
-void encrypt(char *buf, char *b64data, char *rfid)
-{
-  byte cipher[1000];
-  byte iv[N_BLOCK];
-
-  generate_iv(iv);
-
-  snprintf(buf, sizeof buf, "%s,%s,%s,%s,%s,%s\0'", odoo_host, odoo_port, odoo_user, odoo_password, odoo_database,rfid);//, rfidstr);
-  Serial.println(" Input message: " + String(buf));
-
-  base64_encode(b64data, (char *)iv, N_BLOCK);
-  String IV_base64 = String(b64data);
-  Serial.println(" IV b64: " + IV_base64);
-
-  int b64len = base64_encode(b64data, (char *)buf, strlen(buf));//msg.length());
-
-  Serial.println(" The lenght is:  " + String(b64len));
-  aes.set_key(key, sizeof(key));
-  // Encrypt! With AES128, our key and IV, CBC and pkcs7 padding
-  aes.do_aes_encrypt((byte *)b64data, b64len, cipher, key, 128, iv);
-
-  Serial.println("Cipher size: " + String(aes.get_size()));
-
-  base64_encode(b64data, (char *)cipher, aes.get_size());
-  Serial.println("Encrypted data in base64: " + String(b64data));
-
-  decrypt(b64data, IV_base64, aes.get_size(),strlen(buf));
-}
-
-void decrypt(String b64data, String IV_base64, int size, int buf_size)
-{
-  char data_decoded[200];
-  char iv_decoded[200];
-  byte out[200];
-  char temp[200];
-  b64data.toCharArray(temp, 200);
-  base64_decode(data_decoded, temp, b64data.length());
-  IV_base64.toCharArray(temp, 200);
-  base64_decode(iv_decoded, temp, IV_base64.length());
-  aes.do_aes_decrypt((byte *)data_decoded, size, out, key, 128, (byte *)iv_decoded);
-  char message[buf_size];//msg.length()];
-  base64_decode(message, (char *)out, b64data.length());
-  printf("Out %s \n", message);
-  Serial.println();
-  Serial.println();
-}
-
 
